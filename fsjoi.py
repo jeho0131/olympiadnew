37cd3136b5118053abfde7d0c77968158aa7a23b
jeho0131 <64771576+jeho0131@users.noreply.github.com>
2021-04-20 19:00:46 +0900

    Add files via upload

--- fsjoi.py
+++ fsjoi.py
@@ -9,13 +9,36 @@ for i in range(y):
         a[i].append(aa[j])
 
 SJOI = ['S','J','O','I']
+find = ['','','','']
 count = 0
+
+def findf(n):
+    for i in range(n,4):
+        find[i] = ''
 
 def sjoi(f, fx, fy):
     global count
+    
+    if f < 3:
+        if a[fy][fx] == SJOI[f] or f == 0:
+            if fy != 0:
+                sjoi(f+1, fx, fy-1)
+                
+            if fx != 0:
+                sjoi(f+1, fx-1, fy)
+                
+            if fy != y-1:
+                sjoi(f+1, fx, fy+1)
+                
+            if fx != x-1:
+                sjoi(f+1, fx+1, fy)
+   
+    if f == 3 and a[fy][fx] == 'I':
+        count += 1
 
-    if f < 4:
-        if
-
-    if f == 0 and (fx :
-        
+for fsy in range(y):
+    for fsx in range(x):
+        if a[fsy][fsx] == SJOI[0]:
+            sjoi(0, fsx, fsy)
+            
+print(count)
